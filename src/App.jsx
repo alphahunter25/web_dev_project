e4c4714642ec8acb6ee7faf84fa208a70268efaf
alphahunter25 <ahmedrauf121@gmail.com>
2025-01-23 23:12:04 +0500

    updated paths

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
 
   
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <Routes>
         <Route index element={<Signup />} />
         <Route path="/signup" element={<Signup/>} />
